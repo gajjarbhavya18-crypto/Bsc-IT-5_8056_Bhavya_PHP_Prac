4c556f9e1c478cd8cfbe5461309f8ba1a33418e1
gajjarbhavya18-crypto <gajjarbhavya18@gmail.com>
2026-07-07 21:51:24 +0530

    Add files via upload

---
+++ array_unshift.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo"<hr><h3>4 - array_unshift()</h3>";
+            $num = array(20,30,40);
+            array_unshift($num);
+    ?>
+</body>
+</html>
